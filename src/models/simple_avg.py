24a42736622cdbdc1ee8e9b5e55087cec2ca1de6
pedroig <pedroigorao@hotmail.com>
2018-04-25 02:12:52 +0000

    more dimensions for word embedding

--- src/models/simple_avg.py
+++ src/models/simple_avg.py
@@ -6,22 +6,22 @@ from ..datasetAPI import RotaDosConcursos
 
 class SimpleAvg:
 
-    def __init__(self):
-        self.trainObj = RotaDosConcursos(subset='train')
-        self.testObj = RotaDosConcursos(subset='test')
+    def __init__(self, n_features_per_word=50, random_state=1):
+        self.trainObj = RotaDosConcursos(subset='train', random_state=random_state)
+        self.testObj = RotaDosConcursos(subset='test', random_state=random_state)
 
         self.target_names = self.trainObj.target_names
-        self.nCategories = len(self.target_names)
+        self.n_categories = len(self.target_names)
 
-        self.nFeaturesPerWord = 50
+        self.n_features_per_word = n_features_per_word
         wordEmbedPath = 'dataset/glove/glove_s{}.txt'.format(
-            self.nFeaturesPerWord)
+            self.n_features_per_word)
         wordEmbedModel = KeyedVectors.load_word2vec_format(
             wordEmbedPath,
             unicode_errors="ignore")
 
-        self.X_train_avg = self.vector_sentence_to_avg(self.trainObj, wordEmbedModel)
-        self.X_test_avg = self.vector_sentence_to_avg(self.testObj, wordEmbedModel)
+        self.X_train_avg = self.sentence_to_avg(self.trainObj, wordEmbedModel)
+        self.X_test_avg = self.sentence_to_avg(self.testObj, wordEmbedModel)
 
     def row_sentence_to_avg(self, row, wordEmbedModel, answer_list):
         """
@@ -32,13 +32,11 @@ class SimpleAvg:
 
         words = row.clean_text.lower().split()
 
-        nFeaturesPerWord = len(wordEmbedModel.word_vec('casa'))
-
-        avg = np.zeros((nFeaturesPerWord,))
+        avg = np.zeros((self.n_features_per_word,))
         total = len(words)
-        for w in words:
+        for word in words:
             try:
-                avg += wordEmbedModel.word_vec(w)
+                avg += wordEmbedModel.word_vec(word)
             except KeyError:
                 total -= 1
         if total != 0:
@@ -47,16 +45,16 @@ class SimpleAvg:
             print("Clean text with no words in the embedding model for index {} .".format(row.name))
         answer_list.append(avg)
 
-    def vector_sentence_to_avg(self, dataObj, wordEmbedModel):
+    def sentence_to_avg(self, dataObj, wordEmbedModel):
         X_avg = []
         dataObj.df.apply(self.row_sentence_to_avg, axis=1, args=[wordEmbedModel, X_avg])
         X_avg = np.array(X_avg)
         return X_avg
 
-    def simple_model(self):
-        X_input = keras.layers.Input(shape=(self.nFeaturesPerWord,))
+    def build_model(self):
+        X_input = keras.layers.Input(shape=(self.n_features_per_word,))
 
-        X = keras.layers.Dense(self.nCategories, name='fc')(X_input)
+        X = keras.layers.Dense(self.n_categories, name='fc')(X_input)
         X = keras.layers.Activation('softmax')(X)
         model = keras.models.Model(
             inputs=X_input,
@@ -69,7 +67,7 @@ class SimpleAvg:
         return self.target_names[categoryNum]
 
     def execute_model(self):
-        model = self.simple_model()
+        model = self.build_model()
         model.summary()
 
         model.compile(
@@ -95,7 +93,7 @@ class SimpleAvg:
 
     def inspect_mispredictions(self, model, dataObj, X_avg, max_inspect_number):
         pred = model.predict(X_avg)
-        mispredictions = 0
+        mispredictions_count = 0
         for i in range(len(dataObj.target)):
             categoryNum = np.argmax(pred[i])
             if self.num_to_label(categoryNum) != dataObj.target.iloc[i]:
@@ -103,6 +101,6 @@ class SimpleAvg:
                 print('\nExpected category: {}\nPrediction: {}'.format(
                     dataObj.target.iloc[i],
                     self.num_to_label(categoryNum)))
-                mispredictions += 1
-                if mispredictions > max_inspect_number:
+                mispredictions_count += 1
+                if mispredictions_count > max_inspect_number:
                     break
