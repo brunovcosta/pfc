fb0c2b6d298566b8b6216cc42980ec25aacb3852
pedroig <pedroigorao@hotmail.com>
2018-07-01 03:29:07 +0000

    Report and README update

---
+++ test/testing_bow.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+module_root = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, os.path.abspath(module_root))
+
+from src.models.bag_of_words.classifiers import *
+
+
+model = SVM(
+    random_state=1,
+    frac=0.001,
+    dict_name="default.json",
+    min_number_per_label=1000)
+model.fit(save_metrics=False)
+model.inspect_mispredictions('test', 3)
