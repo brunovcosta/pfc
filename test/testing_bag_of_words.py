58751baf76a0f2b8187ec70d579bda7517eca3cb
Lucas Ricarte <lucasrrt@gmail.com>
2018-07-07 18:40:12 -0300

    pedroig linter

--- test/testing_bag_of_words.py
+++ test/testing_bag_of_words.py
@@ -1,12 +1,10 @@
 import os
 import sys
-print("WAHEY, it just began")
+
 module_root = os.path.join(os.path.dirname(__file__), '..')
 sys.path.insert(0, os.path.abspath(module_root))
-print("WAHEY, it just started")
 
 from src.models.bag_of_words.classifiers import SVM
-print("WAHEY, it just almost ending")
 
 
 model = SVM(
@@ -16,4 +14,3 @@ model = SVM(
     min_number_per_label=1000)
 model.fit(save_metrics=True)
 model.inspect_mispredictions('test', 3)
-print("WAHEY, it just end")
