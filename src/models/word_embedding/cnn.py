b3d19a64d5c704afe6d37add7d9f4f6384eee243
Bruno Costa <brunocosta@desc-mac-n178.local>
2018-07-07 13:57:46 -0300

    cnn

--- src/models/word_embedding/cnn.py
+++ src/models/word_embedding/cnn.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+from gensim.models import KeyedVectors
 from .word_embedding_model import WordEmbeddingModelKeras
 
 """
@@ -12,15 +13,29 @@ http://www.aclweb.org/anthology/D14-1181
 class CNN(WordEmbeddingModelKeras):
 
     def _build_X_input(self, dataObj):
-        pass
-        # return np.array
+        print("build X input")
+        train_data = dataObj
+        train_data.df = train_data.df.iloc[:25]
+        train_categories_len = len(train_data.target_names)
+
+        sentence_vectors = []
+        try:
+            words = [self.wordEmbedModel.word_vec(word) for word in word_tokenize(sentence.lower())]
+            sentence_vectors.append(words)
+        except KeyError:
+            pass
+        except AttributeError:
+            print(sentence)
+
+        self.max_sentence_size = max([len(sentence) for sentence in sentence_vectors])
+
+        normalized_input = [np.concatenate((sentence,np.zeros((max_sentence_size - len(sentence),dimensions)))) for sentence in sentence_vectors]
+        return normalized_input
+
 
     def _build_model(self):
-        pass    
+        print("build model")
         # return keras model
-        """
-        word_vectors = [word_embed_model[word] for word in train_data.text]
-
         batch_size = 32 # in each iteration, we consider 32 training examples at once
         num_epochs = 200 # we iterate 200 times over the entire training set
         kernel_size = 3 # we will use 3x3 kernels throughout
@@ -30,43 +45,13 @@ class CNN(WordEmbeddingModelKeras):
         drop_prob_1 = 0.25 # dropout after pooling with probability 0.25
         drop_prob_2 = 0.5 # dropout in the FC layer with probability 0.5
         hidden_size = 512 # the FC layer will have 512 neurons
-        """
-
-        """
-        import keras
-        import numpy as np
-        from src.datasetAPI import RotaDosConcursos
-        from gensim.models import KeyedVectors
-        from nltk.tokenize import word_tokenize
 
         dimensions = 50
         categories_count = 50
 
-        train_data = RotaDosConcursos(subset='train')
-        train_data.df = train_data.df.iloc[:25]
-        #test_data = RotaDosConcursos(subset='test')
 
-        train_categories_len = len(train_data.target_names)
 
-        word_embed_path = 'dataset/glove/glove_s%d.txt'%dimensions
-        word_embed_model = KeyedVectors.load_word2vec_format(
-            word_embed_path,
-            unicode_errors="ignore")
-
-        sentence_vectors = []
-        for sentence in train_data.clean_text:
-            try:
-                sentence_vectors.append([word_embed_model[word] for word in word_tokenize(sentence.lower())])
-            except KeyError:
-                pass
-            except AttributeError:
-                print(sentence)
-
-        max_sentence_size = max([len(sentence) for sentence in sentence_vectors])
-
-        normalized_input = [np.concatenate((sentence,np.zeros((max_sentence_size - len(sentence),dimensions)))) for sentence in sentence_vectors]
-
-        input_layer = keras.Input([max_sentence_size,dimensions])
+        input_layer = keras.Input([self.max_sentence_size,dimensions])
 
         filter_layer_4_1 = keras.layers.Conv1D(1, 4, activation='relu')(input_layer)
         filter_layer_4_2 = keras.layers.Conv1D(1, 4, activation='relu')(input_layer)
@@ -77,27 +62,23 @@ class CNN(WordEmbeddingModelKeras):
         filter_layer_2_1 = keras.layers.Conv1D(1, 2, activation='relu')(input_layer)
         filter_layer_2_2 = keras.layers.Conv1D(1, 2, activation='relu')(input_layer)
 
-        max_layer_4_1 = keras.layers.MaxPooling1D(max_sentence_size - 4 + 1)(filter_layer_4_1)
-        max_layer_4_2 = keras.layers.MaxPooling1D(max_sentence_size - 4 + 1)(filter_layer_4_2)
+        max_layer_4_1 = keras.layers.MaxPooling1D(self.max_sentence_size - 4 + 1)(filter_layer_4_1)
+        max_layer_4_2 = keras.layers.MaxPooling1D(self.max_sentence_size - 4 + 1)(filter_layer_4_2)
 
-        max_layer_3_1 = keras.layers.MaxPooling1D(max_sentence_size - 3 + 1)(filter_layer_3_1)
-        max_layer_3_2 = keras.layers.MaxPooling1D(max_sentence_size - 3 + 1)(filter_layer_3_2)
+        max_layer_3_1 = keras.layers.MaxPooling1D(self.max_sentence_size - 3 + 1)(filter_layer_3_1)
+        max_layer_3_2 = keras.layers.MaxPooling1D(self.max_sentence_size - 3 + 1)(filter_layer_3_2)
 
-        max_layer_2_1 = keras.layers.MaxPooling1D(max_sentence_size - 2 + 1)(filter_layer_2_1)
-        max_layer_2_2 = keras.layers.MaxPooling1D(max_sentence_size - 2 + 1)(filter_layer_2_2)
+        max_layer_2_1 = keras.layers.MaxPooling1D(self.max_sentence_size - 2 + 1)(filter_layer_2_1)
+        max_layer_2_2 = keras.layers.MaxPooling1D(self.max_sentence_size - 2 + 1)(filter_layer_2_2)
 
         concat1max = keras.layers.Concatenate(axis=1)([max_layer_4_1,
-                                                    max_layer_4_2,
-                                                    max_layer_3_1,
-                                                    max_layer_3_2,
-                                                    max_layer_2_1,
-                                                    max_layer_2_2])
+            max_layer_4_2,
+            max_layer_3_1,
+            max_layer_3_2,
+            max_layer_2_1,
+            max_layer_2_2])
 
         output_layer = keras.layers.Dense(categories_count)(concat1max)
 
 
-        model = keras.Model(input_layer,output_layer)
-
-        model.summary()
-
-        """
+        return keras.Model(input_layer,output_layer)
