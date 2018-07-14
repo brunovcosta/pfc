e571ae0db77503048173ebe796adcfabe11591ba
pedroig <pedroigorao@hotmail.com>
2018-07-14 14:57:38 +0000

    cnn modification for the new interface

--- src/models/word_embedding/cnn.py
+++ src/models/word_embedding/cnn.py
@@ -1,3 +1,4 @@
+from .word_embedding_model import WordEmbeddingModelKeras
 import numpy as np
 import tensorflow as tf
 Input = tf.keras.layers.Input
@@ -14,10 +15,6 @@ Concatenate = tf.keras.layers.Concatenate
 ModelCheckPoint = tf.keras.callbacks.ModelCheckpoint
 Adam = tf.keras.optimizers.Adam
 Model = tf.keras.models.Model
-
-from gensim.models import KeyedVectors
-from .word_embedding_model import WordEmbeddingModelKeras
-from nltk.tokenize import word_tokenize
 
 
 """
@@ -36,31 +33,30 @@ class CNN(WordEmbeddingModelKeras):
         train_data = dataObj
 
         sentence_vectors = []
-        for sentence in train_data.clean_text:
-            words = []
-            for word in word_tokenize(sentence.lower()):
+        for sentence in train_data.splitted_text:
+            words_embedding = []
+            for word in sentence:
                 try:
-                    words.append(self.wordEmbedModel.word_vec(word))
-                except KeyError: 
-                    words.append(self.wordEmbedModel.word_vec('<unk>'))
+                    words_embedding.append(self.wordEmbedModel.word_vec(word))
+                except KeyError:
+                    words_embedding.append(self.wordEmbedModel.word_vec('<unk>'))
                 except AttributeError:
                     print(sentence)
-            sentence_vectors.append(words)
+            sentence_vectors.append(words_embedding)
 
-        normalized_input = [np.concatenate((sentence,np.zeros((self.max_len - len(sentence),self.n_features_per_word)))) for sentence in sentence_vectors]
+        normalized_input = [np.concatenate((sentence,np.zeros((self.max_text_len - len(sentence),self.n_features_per_word)))) for sentence in sentence_vectors]
         return np.array(normalized_input)
 
-
     def _build_model(self):
-        sequence_length = self.max_len
+        sequence_length = self.max_text_len
         embedding_dim = self.n_features_per_word
-        filter_sizes = [3,4,5]
+        filter_sizes = [3, 4, 5]
         num_filters = 512
         drop = 0.5
 
         inputs = Input(shape=(sequence_length,embedding_dim))
 
-        reshape = Reshape((sequence_length,embedding_dim,1))(inputs)
+        reshape = Reshape((sequence_length, embedding_dim,1))(inputs)
 
         conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
         conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
