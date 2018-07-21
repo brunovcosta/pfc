85a517eced0c6e5089718c2eeb68dc5afadd4ce2
pedroig <pedroigorao@hotmail.com>
2018-07-21 21:51:22 +0000

    testing NB & SVM

---
+++ test/testing_SVM.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+module_root = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, os.path.abspath(module_root))
+
+from src.models.bag_of_words.classifiers import SVM
+
+
+model = SVM(
+    random_state=1,
+    frac=1,
+    dict_name="default.json",
+    min_number_per_label=10000)
+model.fit(save_metrics=True)
+
+model.save_plots()
+
+model.inspect_mispredictions('test', 3)
