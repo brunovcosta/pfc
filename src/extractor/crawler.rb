e5a636f09ce88dc639b7674c35eb98211110b1ac
Lucas Ricarte <lucasrrt@gmail.com>
2018-07-28 11:32:46 -0300

    crawler fused with extractor

--- src/extractor/crawler.rb
+++ src/extractor/crawler.rb
@@ -1,3 +1,6 @@
+require 'nokogiri'
+require 'json'
+
 linkPrefix = "http://rotadosconcursos.com.br/questoes-de-concursos/filtro_avancado?anos[]=2018"
 
 for year in 2017.downto(1998)
@@ -22,8 +25,9 @@ puts "Number of threads: #{numberOfThreads}"
 
 def crawler(maxId, numberOfThreads, threadNumber, linkPrefix)
 	puts "we are in the thread #{threadNumber}"
-	inferiorLimit = threadNumber*maxId/numberOfThreads
-	superiorLimit = inferiorLimit + maxId/numberOfThreads - 1 
+	fileCounter = 0
+	nullCounter = 0
+
 	threadNumber.step(maxId-1, numberOfThreads) do |fileNumber|
 		htmlFile = "#{fileNumber}.html"
 		puts "File " + htmlFile
@@ -34,15 +38,40 @@ def crawler(maxId, numberOfThreads, threadNumber, linkPrefix)
 		
 		if extractedText == "OOOPS!"
 			puts "File #{htmlFile} was null"
-			$nullCounter = $nullCounter + 1
-			puts "Current fraction of null pages is around #{100*$nullCounter/(maxId)} %"
+
+			nullCounter = nullCounter + 1
 		else
+			fileCounter = fileCounter + 1
 			jsonFile = "#{fileNumber}.json"
-			`ruby ../../src/extractor/extractor.rb #{fileNumber%1000}/"#{htmlFile}" #{fileNumber%1000}/"#{jsonFile}"`
+			extractor("#{fileNumber%1000}/#{htmlFile}", "#{fileNumber%1000}/#{jsonFile}")
 		end
 		
 		`rm #{fileNumber%1000}/"#{htmlFile}"`
+
+		puts "Thread: #{threadNumber}; File Count: #{fileCounter}; Null Count: #{nullCounter} (Total: #{fileCounter + nullCounter})"
 	end
+end
+
+def extractor(input_path, output_path)
+	input_file = open input_path
+	output_file = File.new output_path, "w"
+
+	html = Nokogiri::HTML input_file.read
+
+	output_data = {
+		id: (input_path.scan /\d+/).last,
+		text: (html.css ".panel-questao .panel-heading").text.gsub(/\n/,'').squeeze(" "),
+		subject_path: (html.css ".materia b").map{|b| b.text.gsub(/\n/,'').squeeze(" ")},
+		alternatives: (html.css ".panel-questao .panel-body ul li").map{|li|li.text.gsub(/\n/,'').squeeze(" ").gsub(/^ ?[A-Z].\ /,'')},
+		image_count: (html.css ".panel-questao .panel-heading img").count,
+		concurso: (html.xpath "//div/p/a[contains(@href,'/concurso/')]").text.gsub(/\n/,'').squeeze(" "),
+		prova: (html.xpath "//div/p/a[contains(@href,'/prova/')]").text.gsub(/\n/,'').squeeze(" "),
+		banca: (html.xpath "//div/p/a[not(contains(@href,'/prova/') or contains(@href,'/concurso/'))]").text.gsub(/\n/,'').squeeze(" "),
+		nivel: (html.xpath "//p[contains(text(),'Nível: ')]").text.gsub(/\n/,'').squeeze(" ").split(': ')[1],
+		answer: ("ABCDE".index (html.css '.resposta-correta-escondida span').text.gsub(/\n|\ /,'')[0])
+	}
+
+	output_file.write output_data.to_json
 end
 
 threads = []
