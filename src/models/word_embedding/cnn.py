9b81dd6c7e454f3a757756d2cf1261035a9a7ee2
Bruno Vieira Costa <brunovcosta@outlook.com>
2018-07-28 11:15:31 -0300

    cnn fix

--- src/models/word_embedding/cnn.py
+++ src/models/word_embedding/cnn.py
@@ -1,20 +1,7 @@
 from .word_embedding_model import WordEmbeddingModelKeras
 import numpy as np
 import tensorflow as tf
-Input = tf.keras.layers.Input
-Sequential = tf.keras.models.Sequential
-Dense = tf.keras.layers.Dense
-Embedding = tf.keras.layers.Embedding
-Conv2D = tf.keras.layers.Conv2D
-MaxPool2D = tf.keras.layers.MaxPool2D
-Reshape = tf.keras.layers.Reshape
-Flatten = tf.keras.layers.Flatten
-Dropout = tf.keras.layers.Dropout
-Permute = tf.keras.layers.Permute
-Concatenate = tf.keras.layers.Concatenate
-ModelCheckPoint = tf.keras.callbacks.ModelCheckpoint
-Adam = tf.keras.optimizers.Adam
-Model = tf.keras.models.Model
+from tensorflow import keras
 
 
 """
@@ -28,47 +15,62 @@ https://github.com/bhaveshoswal/CNN-text-classification-keras
 
 class CNN(WordEmbeddingModelKeras):
 
+    def _row_sentences_to_indices(self, row, answer_list):
+        """
+        Converts a list of strings into a list of indices corresponding
+        to words in the word embedding model.
+        """
+        X_indices = []
+        for word in row.splitted_text:
+            try:
+                word_index = self.wordEmbedModel.vocab[word].index
+            except KeyError:
+                word_index = self.wordEmbedModel.vocab['<unk>'].index
+            X_indices.append(word_index)
+
+        answer_list.append(X_indices)
+
     def _build_X_input(self, dataObj):
-        print("build X input")
-        train_data = dataObj
+        X_indices = []
 
-        sentence_vectors = []
-        for sentence in train_data.splitted_text:
-            words_embedding = []
-            for word in sentence:
-                try:
-                    words_embedding.append(self.wordEmbedModel.word_vec(word))
-                except KeyError:
-                    words_embedding.append(self.wordEmbedModel.word_vec('<unk>'))
-                except AttributeError:
-                    print(sentence)
-            sentence_vectors.append(words_embedding)
+        dataObj.df.apply(
+            self._row_sentences_to_indices, axis=1,
+            args=[X_indices])
 
-        normalized_input = [np.concatenate((sentence,np.zeros((self.max_text_len - len(sentence),self.n_features_per_word)))) for sentence in sentence_vectors]
-        return np.array(normalized_input)
+        X_indices = tf.keras.preprocessing.sequence.pad_sequences(
+            X_indices,
+            maxlen=self.padded_length,
+            padding='pre',
+            truncating='post')
+        return X_indices
+
+    def pretrained_embedding_layer(self):
+        """
+        Creates a Keras Embedding() layer and loads in pre-trained GloVe.
+
+        Returns:
+        embedding_layer -- pretrained layer Keras instance
+        """
+        vocab_len = len(self.wordEmbedModel.vocab)
+
+        embedding_layer = tf.keras.layers.Embedding(
+            input_dim=vocab_len,
+            output_dim=self.n_features_per_word,
+            input_length=self.padded_length)
+        embedding_layer.trainable = False
+        embedding_layer.build((None,))
+        embedding_layer.set_weights([self.wordEmbedModel.vectors])
+
+        return embedding_layer
 
     def _build_model(self):
-        sequence_length = self.max_text_len
-        embedding_dim = self.n_features_per_word
-        filter_sizes = [3, 4, 5]
-        num_filters = 512
-        drop = 0.5
-
-        inputs = Input(shape=(sequence_length,embedding_dim))
-
-        reshape = Reshape((sequence_length, embedding_dim,1))(inputs)
-
-        conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
-        conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
-        conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
-
-        maxpool_0 = MaxPool2D(pool_size=(sequence_length - filter_sizes[0] + 1, 1), strides=(1,1), padding='valid')(conv_0)
-        maxpool_1 = MaxPool2D(pool_size=(sequence_length - filter_sizes[1] + 1, 1), strides=(1,1), padding='valid')(conv_1)
-        maxpool_2 = MaxPool2D(pool_size=(sequence_length - filter_sizes[2] + 1, 1), strides=(1,1), padding='valid')(conv_2)
-
-        concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
-        flatten = Flatten()(concatenated_tensor)
-        dropout = Dropout(drop)(flatten)
-        output = Dense(units=self.n_categories, activation='softmax')(dropout)
-
-        return Model(inputs=inputs, outputs=output)
+        return keras.models.Sequential([
+            tf.keras.layers.Input(shape=(self.padded_length,), dtype='int32'),
+            self.pretrained_embedding_layer(),
+            keras.layers.Conv2D(10, (3,self.n_features_per_word), activation='relu'),
+            keras.layers.MaxPooling2D(),
+            keras.layers.Conv2D(10, (3,self.n_features_per_word), activation='relu'),
+            keras.layers.MaxPooling2D(),
+            keras.layers.Flatten(),
+            keras.layers.Dense(self.n_categories, activation='softmax')
+        ])
