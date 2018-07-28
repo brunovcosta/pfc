87052c1e9e56f8016d665f6bbd05f0204a1a8d61
Bruno Vieira Costa <brunovcosta@outlook.com>
2018-07-28 15:23:24 -0300

    cnn setup

---
+++ setup_cnn.sh
@@ -0,0 +1,16 @@
+# download code
+git clone https://github.com/brunovcosta/PFC
+pip install -r PFC/requirements.txt
+python PFC/nltk_download.py
+
+# download glove
+wget http://143.107.183.175:22980/download.php?file=embeddings/glove/glove_s50.zip -O glove_50.zip
+wget https://s3.us-east-2.amazonaws.com/pfc-ime/rota_dos_concursos.csv -O PFC/dataset/rota_dos_concursos.csv
+unzip glove_50.zip
+mv glove_s50.txt PFC/dataset/glove
+
+# run tests
+cd PFC
+python test/testing_cnn.py
+
+aws s3 sync PFC/logs s3://pfc-ime/logs
